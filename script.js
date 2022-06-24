a6a11122ebc469090ded96eb0b5b4a6aeb89a6bf
Rachiee <105196174+Rachiee@users.noreply.github.com>
2022-06-24 23:51:29 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const operator = prompt("Enter operator: +, -, *, or /");
+
+const number1 = parseInt(prompt("Enter First number: "))
+const number2 = parseInt(prompt("Enter Second Number: "))
+
+let result;
+
+if (operator == "+"){
+  result = number1 + number2;
+}
+else if (operator =="-"){
+  result = number1 - number2;
+}
+else if (operator == "*"){
+  result = number1 * number2;
+}
+else if (operator == "/"){
+  result = number1 / number2;
+}
+else {
+  document.write("invalid function")
+}
+
+alert(result);
+//console.log('${number1} ${operator} ${number2} = ${result}');
